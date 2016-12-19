12cad959da1ab433702f66d59a80e0684e3c380b
zenglue <keonilrath@gmail.com>
2016-12-19 12:51:36 -0500

    initial attrs

--- db/migrate/20161219173607_create_users.rb
+++ db/migrate/20161219173607_create_users.rb
@@ -1,6 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
+      t.string :user_name
+      t.integer :role, defualt: 0
 
       t.timestamps
     end
