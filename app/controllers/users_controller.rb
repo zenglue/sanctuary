122b3b0296af438f0ff4ee9f7999926a6170dcdb
zenglue <keonilrath@gmail.com>
2016-12-21 11:14:07 -0500

    rough controller methods finished, views init

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,47 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @users = User.all
+
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+    redirect_to user_path(@user)
   end
 
   def edit
   end
 
   def update
+    if current_user.id == @user.id || current_user.admin?
+      @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      redirect_to '/', error: "Access denied"
+    end
   end
 
   def destroy
+    @user.destroy
+    redirect_to users_path, notice: "User account removed"
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :location, :organization, :services)
   end
 end
