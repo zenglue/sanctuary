6bafd3900d0f34e9edbeb10a31bfa54b31372d98
zenglue <keonilrath@gmail.com>
2016-12-31 03:10:12 -0500

    got rid of double paste

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -77,29 +77,7 @@ class CitiesController < ApplicationController
       end
     end
   end
-
-  def upvote
-    @city = City.find(params[:id])
-    @vote = @city.votes.create(vote_type: 1, user_id: current_user.id)
-    if @vote.save
-      @city.update_vote_count
-      redirect_to city_path(@city), notice: "Upvoted City"
-    else
-      redirect_to city_path(@city), alert: "Can only cast one vote per city, or voting is currently limited to signed in users"
-    end
-  end
-
-  def downvote
-    @city = City.find(params[:id])
-    @vote = @city.votes.create(vote_type: 0, user_id: current_user.id)
-    if @vote.save
-      @city.update_vote_count
-      redirect_to city_path(@city), notice: "Downvoted City"
-    else
-      redirect_to city_path(@city), alert: "Can only cast one vote per city, or voting is currently limited to signed in users"
-    end
-  end
-
+  
   def destroy
     if current_user.admin?
       @city.destroy
