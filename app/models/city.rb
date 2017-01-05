1264f4610b21db26af6ba202bf3846fb38a165b0
zenglue <keonilrath@gmail.com>
2017-01-05 11:09:33 -0500

    default scope added to city ordering

--- app/models/city.rb
+++ app/models/city.rb
@@ -6,6 +6,7 @@ class City < ApplicationRecord
   scope :possible_sanctuary, -> {where(official_status: 2)}
   scope :non_sanctuary, -> {where(official_status: 1)}
   scope :unknown, -> {where(official_status: 0)}
+  default_scope {order(vote_balance: :desc, vote_count: :desc)}
 
   validates :name, :state, :official_status, presence: true
 
@@ -16,14 +17,6 @@ class City < ApplicationRecord
     City.order(vote_count: :desc)
   end
 
-  def update_vote_count
-    self.increment!(:vote_count)
-  end
-
-  def update_vote_balance
-    self.update(vote_balance: self.vote_balance)
-  end
-
   def upvote_count
     self.votes.where(vote_type: 1).count
   end
@@ -32,10 +25,19 @@ class City < ApplicationRecord
     self.votes.where(vote_type: 0).count
   end
 
+  def update_vote_count
+    self.increment!(:vote_count)
+  end
+
   def vote_balance
     value = self.upvote_count - self.downvote_count
     value
   end
+
+  def update_vote_balance
+    self.update(vote_balance: self.vote_balance)
+  end
+
 
   def rank_by_vote_balance_and_total_votes
     City.order(vote_balance: :desc, vote_count: :desc).find_index(self) + 1
